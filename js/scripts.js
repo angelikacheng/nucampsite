7e199afc55ae9a0003ab27eb08339355774c73e8
Angelika Cheng <angelika.cheng@gmail.com>
2019-10-30 22:50:40 -0400

    Week 4 Assignment

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+/* Carousel */
+$(function(){
+    $(".carousel").carousel({interval: 2000});
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+/* Reserve Button */
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal();
+    });
+/* Login Button */
+    $("#loginButton").click(function(){
+        $("#loginModal").modal();
+    });
+});
+
